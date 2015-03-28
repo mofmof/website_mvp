2504b56529ce4ae4fc982e0aaca2ff9913118e93
Satomi <stm.ngw@gmail.com>
2015-03-28 22:00:28 +0900

    アニメーション追加

--- js/script.js
+++ js/script.js
@@ -11,19 +11,119 @@ $(function(){
 	    return false;
 	  });
 
-    //ブラウザ判定
-	var userAgent = window.navigator.userAgent.toLowerCase();
-    var appVersion = window.navigator.appVersion.toLowerCase();
+    //ファーストビューアニメーション
 
-	//Windowsの場合の背景ストレッチ
-	if (userAgent.indexOf('msie') != -1) {
-	    $(".headline").css( "background-size", "contain" );
-	}
-	
+    $(".logoarea").hide();
+    $(".catch01").hide();
+    $(".catch02").hide();
+    $(".catch03").hide();
+    $(".catch04").hide();
+    $(".area_badge").hide();
+    $(".btn_g").hide();
+    $(".btn_more").hide();
+
+    setTimeout(function(){
+    	$(".logoarea").show("fast");
+    },100)
+
+    setTimeout(function(){
+    	$(".area_badge").show("fast");
+    },1500)
+
+    setTimeout(function(){
+    	$(".catch01").show();
+    	$(".catch01").shuffleLetters();
+    },2000)
+
+    setTimeout(function(){
+    	$(".catch02").show();
+    	$(".catch02").shuffleLetters();	
+    },3000)
+
+    setTimeout(function(){
+    	$(".catch03").show("fast");
+    	$(".catch04").show("fast");
+    },3500)
+
+    setTimeout(function(){
+    	$(".btn_g").show("fast");
+    },4000)
+
+    setTimeout(function(){
+    	$(".btn_more").show("fast");
+    },4300)
+
+    //スクロール値でアニメーション
+
+    var w = $(window).width();
+	if (w > 768) {
+		sca();
+	} 
+
 	//セレクト装飾
-    $(document).ready(function(){
-		$('.myselect').customSelect();
+ //    $(document).ready(function(){
+	// 	$('.myselect').customSelect();
+	// });
+
+function sca() {
+	$(".corner_left").hide();
+	$(".corner_right").hide();
+    $(".text_ex").hide();
+    $(".eximg_wrap").hide();
+    $(".corner_left").hide();
+    $(".corner_right").hide();
+    $(".f01").hide();
+    $(".f02").hide();
+    $(".f03").hide();
+    $(".f04").hide();
+    $(".f05").hide();
+    $(".release").hide();
+    $(".list_note").hide();
+    $(".totop").hide();
+
+    var w = $(window).width();
+    $(window).scroll(function () {
+
+		var s = $(this).scrollTop();
+		console.log(s);
+		var c2 = 350;
+		var c3 = 1000;
+		var c4 = 2300;
+
+		if(s > c2) {
+			$(".text_ex").fadeIn('fast');
+			setTimeout(function(){
+				$(".eximg_wrap").fadeIn('slow');
+				$(".corner_left").fadeIn('slow');
+				$(".corner_right").fadeIn('slow');
+			},300)
+		}
+
+		if (s > c3) {
+			$(".f01").fadeIn('slow');
+			$(".f02").fadeIn('slow');
+			setTimeout(function(){
+				$(".f03").fadeIn('slow');
+				$(".totop").fadeIn('slow');
+			},500)
+			setTimeout(function(){
+				$(".f04").fadeIn('slow');
+				$(".f05").fadeIn('slow');
+			},1500)
+			setTimeout(function(){
+				$(".release").fadeIn('slow');
+			},2000)
+
+		}
+
+		if (s > c4) {
+			$(".list_note").show('fast');
+		}
 	});
+}
+
 
 });
+
+
 
